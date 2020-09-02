b0ce51d5556a2df64ebe5daa2a83c3f10077f667
Sven van Putten <svenvanputten@gmail.com>
2020-09-02 17:33:26 +0200

    Changed the display duration of toast messages and updated the image check to display an image based on the currentThrow instead of the lastThrow

--- app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
+++ app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
@@ -13,7 +13,6 @@ class HigherLowerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_higher_lower)
 
         binding = ActivityHigherLowerBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -43,10 +42,10 @@ class HigherLowerActivity : AppCompatActivity() {
      * Update the last throw text and the dice image resource drawable with the current throw.
      */
     private fun updateUI() {
-        binding.tvLastThrow.text = getString(R.string.last_throw, lastThrow)
+        binding.tvLastThrow.text = getString(R.string.last_throw, currentThrow)
 
-        // Display the correct dice image based on the lastThrow value
-        when (lastThrow) {
+        // Display the correct dice image based on the currentThrow
+        when (currentThrow) {
             1 -> binding.ivDice.setImageResource(R.drawable.dice1);
             2 -> binding.ivDice.setImageResource(R.drawable.dice2);
             3 -> binding.ivDice.setImageResource(R.drawable.dice3);
@@ -63,6 +62,7 @@ class HigherLowerActivity : AppCompatActivity() {
     private fun rollDice() {
         lastThrow = currentThrow
         currentThrow = (1..6).random()
+
         updateUI()
     }
 
@@ -109,13 +109,13 @@ class HigherLowerActivity : AppCompatActivity() {
      * Displays a successful Toast message.
      */
     private fun onAnswerCorrect() {
-        Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+        Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_SHORT).show()
     }
 
     /**
      * Displays an incorrect Toast message.
      */
     private fun onAnswerIncorrect() {
-        Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_LONG).show()
+        Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_SHORT).show()
     }
 }
